d3439a1e4b15a42a6861ed48463d7c1daef17651
James Stevenson <james.sharpsteen@gmail.com>
2019-10-27 22:47:52 -0700

    Edit wording and add exception for empty containers

--- ds_exceptions.py
+++ ds_exceptions.py
@@ -1,3 +1,8 @@
 class EmptyContainerError(Exception):
     def __init__(self, message):
         self.message = message
+
+
+class NoSuchKeyError(Exception):
+    def __init__(self, message):
+        self.message = message
